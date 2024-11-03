ec89d8807a5b145bacdaf96b3d296c5e108e15bc
Lojaina <lojymoataz81@gmail.com>
2024-11-03 16:17:42 +0300

    initial project added empty homeScreen

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:alarm_app/homeScreen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute:HomeScreen.routeName ,
+      routes: {
+        HomeScreen.routeName:(context)=>HomeScreen(),
+      },
+
+    );
+  }
+}
+
+
